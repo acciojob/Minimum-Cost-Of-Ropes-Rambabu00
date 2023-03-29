e969f83bbc9fbc28fcabd3a54161de0c9d555b76
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-29 10:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
   //your code here
   
-  
-  
+  var inputValue=document.querySelector("#rope-lengths").value;
+	var arr=inputValue.split(',').map(Number);
+	while(arr.lenght>1){
+		arr=arr.sort(function (a,b){return a-b});
+			
+			let sum=arr[0]+arr[1];
+		arr.splice(0,2,sum);
+	 
+	document.querySelector("#result").innerHTML=arr;
 }  
+}
